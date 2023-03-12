f7fede481c98efb4e37c4775c1ddf9155a12dd3f
Vladislav.M <lidpid@gmail.com>
2023-03-12 19:28:14 +0300

    task 1 corrected

--- task_10.py
+++ task_10.py
@@ -1,10 +1,15 @@
 coin_count = int(input("Введите число монет: "))
 print("Количество монет:", coin_count)
 coins = []
-flips = 0
+tails = 0
+eagles = 0
 for i in range(0, coin_count):
     coins.append(int(input(f"Задайте положение монеты {i}: ")))
 for i in range(0, coin_count):
     if coins[i] == 0:
-        flips += 1
-print(f"Необходимо перевернуть минимум {flips} монет")
+        tails +=1
+    else: eagles +=1
+if tails >= eagles:
+    print(f"Необходимо перевернуть минимум {eagles} монет")
+else:
+    print(f"Необходимо перевернуть минимум {tails} монет")
