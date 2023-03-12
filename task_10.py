5a7087f1dba4714be3c083d48b4eba4adc8af64c
Vladislav.M <lidpid@gmail.com>
2023-03-12 19:41:02 +0300

    task 10 corrected

--- task_10.py
+++ task_10.py
@@ -1,10 +1,12 @@
+import random
 coin_count = int(input("Введите число монет: "))
 print("Количество монет:", coin_count)
 coins = []
 tails = 0
 eagles = 0
 for i in range(0, coin_count):
-    coins.append(int(input(f"Задайте положение монеты {i}: ")))
+    coins.append(random.randint(0, 1))
+print(coins)
 for i in range(0, coin_count):
     if coins[i] == 0:
         tails +=1
